c518bec0fc942747513342a3d152320a2e1653e2
Fernando Sarabia Ortiz <Fersomn20@gmail.com>
2024-10-16 10:12:50 -0700

    Added delay comment

--- Application.java
+++ Application.java
@@ -6,6 +6,13 @@ public class Application {
         Consumer consumer1 = new Consumer(fifo, "consumer1", 1000);
 
         producer1.start();
+        // this extra if delay is needed
+        /* try {
+            Thread.sleep(500); // Wait for 500 milliseconds
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt(); // Restore the interrupted status
+        }
+        */
         producer2.start();
         consumer1.start();
     }
